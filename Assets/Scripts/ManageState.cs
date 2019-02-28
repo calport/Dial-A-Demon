d6ff88717107cc4b121758abbefe9bf58af7bf03
Bri <bs3864@nyu.edu>
2019-02-28 15:43:30 -0500

    Added Menu Screen Sprites and Buttons

--- Assets/Scripts/ManageState.cs
+++ Assets/Scripts/ManageState.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    MainMenu,
+    ProtectionCircle,
+    GameSettings,
+    OpeningRitual, 
+    Texting,
+    Contract,
+    PhoneDial,
+    PhoneRinging,
+    Party
+    
+}
 
 public class ManageState : MonoBehaviour
 {
+    private GameState currentState;  
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = GameState.MainMenu;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        checkGameState();
     }
+
+    public void checkGameState()
+    {
+        switch (currentState)
+        {
+            case GameState.MainMenu:
+                break;
+            case GameState.ProtectionCircle:
+                break;
+            
+        }
+    } 
 }
