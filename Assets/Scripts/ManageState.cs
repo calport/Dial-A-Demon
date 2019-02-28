9d6a5bd797091df9d72e703e77a8673b81bdf0b0
Bri <bs3864@nyu.edu>
2019-02-28 17:16:55 -0500

    Main menu buttons for messages and calling to move you to those screens.

--- Assets/Scripts/ManageState.cs
+++ Assets/Scripts/ManageState.cs
@@ -19,12 +19,35 @@ public enum GameState
 
 public class ManageState : MonoBehaviour
 {
-    private GameState currentState;  
+    private GameState currentState;
+    
+    public GameObject mainMenu;
+    public GameObject textingScreen;
+    public GameObject phoneDial; 
+    
+    //Main Menu Buttons
+    private Button goToTexting;
+    private Button goToDial; 
     
     // Start is called before the first frame update
     void Start()
     {
+        //mainMenu = GameObject.Find("MainMenu_UI");
+        //textingScreen = GameObject.Find("Keyboard");
+        //phoneDial = GameObject.Find("PhoneDial");
+        
         currentState = GameState.MainMenu;
+        
+        //Texting Button
+        goToTexting = GameObject.Find("GoToTexting").GetComponent<Button>();
+        goToTexting.onClick.AddListener(ToTexting);
+        
+        //Phone Call Button
+        goToDial = GameObject.Find("GoToDial").GetComponent<Button>();
+        goToDial.onClick.AddListener(GoToDial);
+
+        //Setting Button
+
     }
 
     // Update is called once per frame
@@ -38,10 +61,38 @@ public class ManageState : MonoBehaviour
         switch (currentState)
         {
             case GameState.MainMenu:
+                
                 break;
             case GameState.ProtectionCircle:
                 break;
-            
+            case GameState.GameSettings:
+                break;
+            case GameState.OpeningRitual:
+                break;
+            case GameState.Texting:
+                mainMenu.SetActive(false);
+                textingScreen.SetActive(true);
+                break;
+            case GameState.Contract:
+                break;
+            case GameState.PhoneDial:
+                mainMenu.SetActive(false);
+                phoneDial.SetActive(true);
+                break;
+            case GameState.PhoneRinging:
+                break;
+            case GameState.Party:
+                break;
         }
-    } 
+    }
+
+    void ToTexting()
+    {
+        currentState = GameState.Texting;
+    }
+    
+    void GoToDial()
+    {
+        currentState = GameState.PhoneDial;
+    }
 }
