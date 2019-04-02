34bc11f62940d7ab93ff62829e50d44e189b09a9
yukii <yukiizzzx@gmail.com>
2019-04-02 16:26:04 -0400

    Built the basic construction of the calendar system and add the UI moving with finger

---
+++ Assets/Scripts/PlotManage/Plot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public abstract class Plot
+{
+    public float Day;
+    public float Hour;
+    public float Second;
+    
+    //This will appear in every subclass so object can set time when they are created
+    /*
+
+    public Plot(float day, float hour, float second)
+    {
+        _day = day;
+        _hour = hour;
+        _second = second;
+    }*/
+    
+    public virtual void Init(){}
+    public virtual void Update(){}
+    
+    public virtual void Clean(){}
+    
+    public virtual void RearrangeTime(){}
+}
+
+public class TextPlot : Plot
+{
+    public TextPlot(float day, float hour, float second)
+    {
+        Day = day;
+        Hour = hour;
+        Second = second;
+    }
+    
+}
