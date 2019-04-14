add1b8c4f18f1e8aabda7d50a96ef0a113a95d2d
yukii <yukiizzzx@gmail.com>
2019-04-14 15:25:56 -0400

    - solve the bug with the moving controller - solve the bug of cannot have all selections pump up - add vibration - add the simple "..." UI in conversation - solve the bug of the dialogue pumps up with weird shaking

--- Assets/Scripts/DialogueSystem/KeyboardInput.cs
+++ Assets/Scripts/DialogueSystem/KeyboardInput.cs
@@ -15,10 +15,10 @@ public class KeyboardInput : MonoBehaviour
     public string content = String.Empty;
     //private TextInput _textInput;
     public GameObject Pointer;
-    private bool _pointerIn;
+    public bool _pointerIn;
+
     void Awake()
     {
-        
         //_textInput = this.transform.parent.GetComponentInChildren<TextInput>();
         DemonTextDialogueUI.Instance.keyboardArray.Add(keyCode, this);
     }
@@ -27,65 +27,82 @@ public class KeyboardInput : MonoBehaviour
     void Update()
     {
         //change the textbox input when the keyboard is touched 
-        if (string.Compare(String.Empty,content)!=0)
+        if (string.Compare(String.Empty, content)!= 0)
         {
-             //Touch touch = Input.GetTouch(0);
-             if (string.Compare(content,DemonTextDialogueUI.Instance.textBox.text)!=0)
-             {
-                 if (_pointerIn)
-                 {
-                     //vibration
-                     //TODO
-                     DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.ChosenWords,
-                         content, choiceNumber);
-                     Debug.Log(content);
-                 }
-                 else
-                 {
-                     DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.NoWords,
-                         content, choiceNumber);
-                 }
-             }
+            if (string.Compare(content, DemonTextDialogueUI.Instance.textBox.text) != 0)
+            {
+                if (_pointerIn)
+                {
+                    DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.ChosenWords,
+                        content, choiceNumber);
+                    Handheld.Vibrate();
+                    iPhoneUtils.Vibrate();
+                }
+            }
 
-             /*if (touch.phase == TouchPhase.Stationary)
-             {
-                 if (DemonTextDialogueUI.Instance.textBox.text != content)
-                 {
-                     DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.SelectWords, content, choiceNumber);
-                     Debug.Log(content);
-                 }
+            #region comment
+            //if (string.Compare(content,DemonTextDialogueUI.Instance.textBox.text) != 0)
+            //{
+            //    if (_pointerIn)
+            //    {
+            //        //vibration
+            //        //TODO
+            //        DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.ChosenWords,
+            //            content, choiceNumber);
+            //    //Debug.Log(DemonTextDialogueUI.TextBoxState.ChosenWords);
+            //    //Debug.Log(content);
+            //    //Debug.Log(choiceNumber);
+            //    }
+            //    else
+            //    {
+            //        DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.NoWords,
+            //            content, choiceNumber);
+            //    }
+            //}
 
-                 if (touch.pressure > 1.5f)
-                 {
-                     //TODO 
-                     //vibration
-                     DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.ChosenWords, content, choiceNumber );
-                 }
-                 
-             }
-             else
-             {
-                 if (TextInputManager.Instance.boxState == TextInputManager.TextBoxState.SelectWords)
-                 {
-                     TextInputManager.Instance.TextBoxStateChange(TextInputManager.TextBoxState.NoWords, content);
-                 }
-             }*/
-        }      
+            /*if (touch.phase == TouchPhase.Stationary)
+            {
+                if (DemonTextDialogueUI.Instance.textBox.text != content)
+                {
+                    DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.SelectWords, content, choiceNumber);
+                    Debug.Log(content);
+                }
+
+                if (touch.pressure > 1.5f)
+                {
+                    //TODO 
+                    //vibration
+                    DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.ChosenWords, content, choiceNumber );
+                }
+
+            }
+            else
+            {
+                if (TextInputManager.Instance.boxState == TextInputManager.TextBoxState.SelectWords)
+                {
+                    TextInputManager.Instance.TextBoxStateChange(TextInputManager.TextBoxState.NoWords, content);
+                }
+            }*/
+            #endregion
+
+        }
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
+    void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.gameObject == Pointer)
         {
             _pointerIn = true;
-            Debug.Log("Find pointer");
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject == Pointer)
         {
             _pointerIn = false;
-            Debug.Log("Find pointer");
+            DemonTextDialogueUI.Instance.TextBoxStateChange(DemonTextDialogueUI.TextBoxState.NoWords,
+                    content, choiceNumber);
         }
     }
 }
