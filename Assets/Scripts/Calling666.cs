1ca5accaa4602af78a54ecedf9ee685f59ce3c7e
Bri <bs3864@nyu.edu>
2019-04-21 12:34:20 -0400

    Added in sound for calling, for Milestone Demo 2

--- Assets/Scripts/Calling666.cs
+++ Assets/Scripts/Calling666.cs
@@ -12,10 +12,22 @@ public class Calling666 : MonoBehaviour
     //Textbox
     //private TextBox dialTextBox;
     private Text dialText;
+
+    
+    //just for milestone 2 demo
+    public AudioSource DemonRinging;
+    public AudioSource hangingUpSound;
+    public GameObject callScreen;
+    public Button declineCall; 
     
     // Start is called before the first frame update
     void Start()
     {
+        //just for milestone 2 demo 
+        declineCall.onClick.AddListener(hangUp);
+        StartCoroutine(PhoneCall());
+        
+        
         numberButton = GameObject.Find("666_button").GetComponent<Button>();
         numberButton.onClick.AddListener(Dial6);
         
@@ -26,6 +38,27 @@ public class Calling666 : MonoBehaviour
 
     }
 
+    IEnumerator PhoneCall()
+    {
+        yield return 0; 
+        Debug.Log("waited 1 second");
+        
+        yield return new WaitForSeconds(5);
+        Debug.Log("waited for 5 seconds");
+        DemonRinging.Play(0);
+        callScreen.SetActive(true);
+        Debug.Log("started ringing");
+        
+    }
+
+    IEnumerator HangUpCall()
+    {
+        yield return new WaitForSeconds(1);
+        //DemonRinging.Stop();
+        
+        callScreen.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -34,7 +67,14 @@ public class Calling666 : MonoBehaviour
 
     void Dial6()
     {
-        dialText.text = "666";
+        dialText.text = "help";
+    }
+
+    void hangUp()
+    {
+        DemonRinging.Stop();
+        hangingUpSound.Play();
+        StartCoroutine(HangUpCall());
     }
 
 }
