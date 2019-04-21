dd53bd8bf86f7f395837196ed9411d0d775e2f07
Bri <bs3864@nyu.edu>
2019-04-21 14:25:40 -0400

    Working on sounds for the keyboard

---
+++ Assets/Scripts/TextingSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextingSounds : MonoBehaviour
+{
+
+    public AudioSource keyboardSound; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this script is attached");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collision detected");
+        keyboardSound.Play();
+    }
+}
