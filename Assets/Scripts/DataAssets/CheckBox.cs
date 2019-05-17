325267a7658dade28f435163d54da8c996cbbcdf
yukii <yukiizzzx@gmail.com>
2019-05-16 22:28:31 -0400

    - fix the bug of see through - finished the contract - add a remember scriptable obeject to remember the setting

---
+++ Assets/Scripts/DataAssets/CheckBox.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckBox : MonoBehaviour
+{
+
+    [SerializeField]private string dataName;
+    private Data_Contract dataFile;
+    [SerializeField]private GameObject onIcon;
+    [SerializeField]private GameObject offIcon;
+    
+    private void OnEnable()
+    {
+        dataFile = Services.dataContract;
+        if (dataFile.DataBool.ContainsKey(dataName))
+        {
+            bool data;
+            dataFile.DataBool.TryGetValue(dataName, out data);
+            SwitchOnOff(data);
+        }
+    }
+
+
+    // Update is called once per frame
+    public void Click()
+    {
+        if (dataFile.DataBool.ContainsKey(dataName))
+        {
+            bool data;
+            dataFile.DataBool.TryGetValue(dataName, out data);
+            data = !data;
+            dataFile.ChangeData(dataName,data);
+            SwitchOnOff(data);
+        }
+    }
+
+    void SwitchOnOff(bool data)
+    {
+        if (data)
+        {
+            onIcon.SetActive(true);
+            offIcon.SetActive(false);
+        }
+        else
+        {
+            onIcon.SetActive(false);
+            offIcon.SetActive(true);
+        }
+    }
+}
