446835badfbaf65ca47a7f10028464a3fec3fdb0
brianna <bs3864@nyu.edu>
2019-05-21 18:46:49 -0400

    Merge branch 'master' of https://github.com/calport/Dial-A-Demon
    # Conflicts:
    #	Assets/Scenes/Texting.unity

---
+++ Assets/Scripts/OnTapOpenFile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HedgehogTeam.EasyTouch; 
+
+public class OnTapOpenFile : MonoBehaviour
+{
+  
+    
+    private void Start()
+    {
+
+    }
+
+    // Subscribe to events
+    void OnEnable()
+    {
+        EasyTouch.On_SimpleTap += On_SimpleTap;
+    }
+
+    void OnDisable()
+    {
+        UnsubscribeEvent();
+    }
+
+    void OnDestroy()
+    {
+        UnsubscribeEvent();
+    }
+
+    void UnsubscribeEvent()
+    {
+        EasyTouch.On_SimpleTap -= On_SimpleTap;
+    }
+
+    // Simple tap
+    private void On_SimpleTap(Gesture gesture)
+    {
+        
+        // Verification that the action on the object
+        if (gesture.pickedObject == gameObject)
+        {
+           gameObject.GetComponent<OpenFileButton>().OnSimpleTap();
+
+        }
+
+    }
+}
