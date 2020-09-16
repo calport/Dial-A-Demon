af4f849120b8f96d21453209a708d8932ab398aa
Brianna <bs3864@nyu.edu>
2020-09-16 18:21:13 -0400

    Moving with Mouse (for bri Unity Remote)

--- Assets/Scripts/GeneralControlEffect/MovingWithMouse.cs
+++ Assets/Scripts/GeneralControlEffect/MovingWithMouse.cs
@@ -22,18 +22,24 @@ public class MovingWithMouse : MonoBehaviour, IDragHandler
     
     public void OnDrag(PointerEventData eventData)
     { 
+        Debug.Log("On drag is running");
         if (Input.touchSupported)
         {
+            Debug.Log("touch supported");
             if(Input.touchCount == 0 || Input.GetTouch(0).phase == TouchPhase.Began) return;
             var touch = Input.GetTouch(0);
-            Vector3 posChange = touch.position;
-            var plantPos = new Vector3(posChange.x, posChange.y, -1);
+            Vector3 posChange = Camera.main.ScreenToWorldPoint(touch.position);
+            transform.position =  new Vector3(posChange.x,posChange.y,-1);
+            ///var plantPos = new Vector3(posChange.x, posChange.y, -1);
+            Debug.Log("we make it to here");
         }
         else
         {
+            Debug.Log("mouse is being used");
             if(!Input.GetMouseButton(0) || Input.GetMouseButtonDown(0)) return;
             Vector2 mouseDrag = Camera.main.ScreenToWorldPoint(eventData.position);
             transform.position = new Vector3(mouseDrag.x, mouseDrag.y, -1);
+            Debug.Log("we will not make it");
         }
     }
 
